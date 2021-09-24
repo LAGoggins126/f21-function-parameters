c7c050bb8e892b456d1baa181bfe821d9007a618
Jonathan C <jsea@uw.edu>
2021-09-23 17:15:00 -0700

    Added assignment file

---
+++ function-parameters.js
@@ -0,0 +1,10 @@
+// 1) Group instructions on Lines 4-7 below into a new function.
+// 2) Convert the "x" and "y" variables into parameters which
+//      will receive numbers from outside the function.
+let x = 5;
+let y = 2;
+let product = x * y;
+console.log(x + " * " + y + " = " + product);
+
+// 3) Call your new function below 3 separate times with
+//      different supplied x's and y's
